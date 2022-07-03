ef86f8e5f04b164f56395517d4dada7943b4eb5f
ticlacalvin <93556160+ticlacalvin@users.noreply.github.com>
2022-07-02 20:59:37 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "aws_region" {
-default = "us-east-1"
+default = "us-east-2"
 }
 
 variable "key_name" {
-default = "maestrokey"
+default = "ohiokey"
 }
 variable "vpc_cidr" {
 default = "172.0.0.0/24"
@@ -14,14 +14,14 @@ default = ["172.0.0.0/25", "172.0.0.128/25"]
 }
 variable "availability_zones" {
 type = list(string)
-default = ["us-east-1a", "us-east-1b"]
-}
+default = ["us-east-2a", "us-east-2b"]
+
 variable "kubernetes_ami" {
-default = "ami-0b31d0912aa316cd3"
-}
+default = "ami-0960ab670c8bb45f3"
+  
 variable "master_instance_type" {
 default = "t2.medium"
-}
+
 variable "worker_instance_type" {
 default = "t2.micro"
 }
